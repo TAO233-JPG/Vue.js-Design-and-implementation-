f1ddd634c0946eb18604509a3b0f515aec816548
TAO233-JPG <1040581451@qq.com>
2023-11-22 14:55:22 +0800

    代理数组

--- 响应系统/effect.js
+++ 响应系统/effect.js
@@ -1,4 +1,4 @@
-import { INERATE_KEY } from "./reactive.js";
+import { INERATE_KEY, shouldTrack } from "./reactive.js";
 
 const effectStack = [];
 export let aciveEffect;
@@ -26,7 +26,7 @@ export const bucket = new WeakMap();
 
 // 依赖收集
 export const track = (target, key) => {
-  if (!aciveEffect) return;
+  if (!aciveEffect || !shouldTrack) return;
   let depsMap = bucket.get(target);
   if (!depsMap) {
     bucket.set(target, (depsMap = new Map()));
@@ -40,7 +40,7 @@ export const track = (target, key) => {
 };
 
 // 派发更新
-export const trigger = (target, key, type) => {
+export const trigger = (target, key, type, newVal) => {
   const depMap = bucket.get(target);
   if (!depMap) return;
   const deps = depMap.get(key);
@@ -58,6 +58,29 @@ export const trigger = (target, key, type) => {
       });
   }
 
+  // 对于数组添加数据， 需要触发 length 上的 effectFn
+  if (type === "ADD" && Array.isArray(target)) {
+    const lengthEffectFn = depMap.get("length");
+    lengthEffectFn &&
+      lengthEffectFn.forEach((effectFn) => {
+        if (aciveEffect !== effectFn) {
+          effectToRun.add(effectFn);
+        }
+      });
+  }
+
+  // 对于数组，修改了length => newLength，需要取出 index >= newLength 项的effectFn并执行
+  if (Array.isArray(target) && key === "length") {
+    depMap.forEach((dep, idx) => {
+      if (idx >= newVal) {
+        dep.forEach((effectFn) => {
+          if (aciveEffect !== effectFn) {
+            effectToRun.add(effectFn);
+          }
+        });
+      }
+    });
+  }
   deps &&
     deps.forEach((effectFn) => {
       if (effectFn !== aciveEffect) {
--- 响应系统/reactive.js
+++ 响应系统/reactive.js
@@ -5,6 +5,31 @@ import { effect, trigger, track } from "./effect.js";
 export const INERATE_KEY = Symbol("INERATE_KEY");
 export const RAW = Symbol("RAW");
 
+const arrayInstrumentations = {};
+// 数组查找方法重写，确保查找方法可以正确执行
+// 先在代理对象查找，再在原本对象找
+["includes", "indexOf", "lastIndexOf"].forEach((method) => {
+  const originMethod = Array.prototype[method];
+  arrayInstrumentations[method] = function (...args) {
+    // this 是代理对象
+    const res =
+      originMethod.apply(this, args) || originMethod.apply(this[RAW], args);
+    return res;
+  };
+});
+
+export let shouldTrack = true;
+// 在这些方法执行时，屏蔽掉对length跟踪，取消track
+["push", "pop", "shift", "unshift", "splice"].forEach((method) => {
+  const originMethod = Array.prototype[method];
+  arrayInstrumentations[method] = function (...args) {
+    shouldTrack = false;
+    const res = originMethod.apply(this, args);
+    shouldTrack = true;
+    return res;
+  };
+});
+
 const createReactive = (target, isShallow = false, isReadonly = false) => {
   return new Proxy(target, {
     get(target, key, receiver) {
@@ -13,10 +38,15 @@ const createReactive = (target, isShallow = false, isReadonly = false) => {
         return target;
       }
 
+      // 拦截数组方法
+      if (Array.isArray(target) && arrayInstrumentations.hasOwnProperty(key)) {
+        return Reflect.get(arrayInstrumentations, key, receiver);
+      }
+      console.log("-get", key);
       const v = Reflect.get(target, key, receiver);
 
       // 只读数据，不需要依赖收集
-      if (!isReadonly) {
+      if (!isReadonly || typeof key !== "symbol") {
         track(target, key);
       }
 
@@ -33,19 +63,24 @@ const createReactive = (target, isShallow = false, isReadonly = false) => {
       return v;
     },
     set(target, key, newVal, receiver) {
+      console.log("+set", key);
       if (isReadonly) {
         console.warn(`属性 ${key} 是只读的`);
         return true;
       }
       const oldVal = target[key];
-      const triggerType = Object.prototype.hasOwnProperty.call(target, key)
+      const triggerType = Array.isArray
+        ? Number(key) < target.length
+          ? "SET"
+          : "ADD"
+        : Object.prototype.hasOwnProperty.call(target, key)
         ? "SET"
         : "ADD";
       const v = Reflect.set(target, key, newVal, receiver);
       // receiver 是 target 的代理对象
       if (receiver[RAW] === target) {
         if (oldVal !== newVal && (oldVal === oldVal || newVal === newVal)) {
-          trigger(target, key, triggerType);
+          trigger(target, key, triggerType, newVal);
         }
       }
       return v;
@@ -73,14 +108,19 @@ const createReactive = (target, isShallow = false, isReadonly = false) => {
     // 拦截 for...in
     // 在 trigger 中会取与 INERATE_KEY 相关连的 effectfn 执行
     ownKeys(target) {
-      track(target, INERATE_KEY);
+      track(target, Array.isArray(target) ? "length" : INERATE_KEY);
       return Reflect.ownKeys(target);
     },
   });
 };
 
+const reactiveMap = new Map();
 function reactive(target) {
-  return createReactive(target);
+  if (reactiveMap.has(target)) return reactiveMap.get(target);
+
+  const proxy = createReactive(target);
+  reactiveMap.set(target, proxy);
+  return proxy;
 }
 function shallowReactive(target) {
   return createReactive(target, true);
@@ -126,5 +166,16 @@ const readonlyObj = readonly(obj);
 // effect(() => {
 //   console.log(readonlyObj.box.total);
 // });
-
 // readonlyObj.box.total += 2;
+
+/* array length */
+
+const p = reactive([0]);
+
+effect(() => {
+  for (let i of p) {
+  }
+  console.log("-- for of");
+});
+console.log("++ ===-");
+p.push(12);
